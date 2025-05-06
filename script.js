7ea93d3331dd9fc4ced34cfdd21e3dd72ebf1b59
tse1993 <150841317+tse1993@users.noreply.github.com>
2025-05-06 22:32:04 +0300

    script.js

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ function getHumanChoice(){
     let choice = prompt('rock, paper, scissors?')
     return choice.toLowerCase()
 }
+
+function playRound(humanChoice,computerChoice){
+    if (humanChoice === computerChoice){
+        return 'Tie'
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'scissors'){
+        humanScore++
+        return 'Player wins'
+    }else if (humanChoice === 'rock' && computerChoice === 'paper')
+        computerScore++
+        return 'Computer wins'
+    }
+
+    function game() {
+        let rounds = 5
+        for (let i = 0; i < rounds; i++) {
+            let humanChoice = getHumanChoice();
+            if (!humanChoice) {
+                console.log('Game cancelled.');
+                break;
+            }
+            let computerChoice = getComputerChoice();
+            console.log(`Round ${i + 1}:`);
+            console.log(`You chose ${humanChoice}, Computer chose ${computerChoice}`);
+            console.log(playRound(humanChoice, computerChoice));
+            console.log(`Scores => You: ${humanScore}, Computer: ${computerScore}`);
+        }
+    
+        console.log('Game Over!');
+        if (humanScore > computerScore) {
+            console.log('Congratulations, you win!');
+        } else if (humanScore < computerScore) {
+            console.log('Computer wins. Better luck next time');
+        } else {
+            console.log('It\'s a tie');
+        }
+    }
